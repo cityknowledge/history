ce2d08a265a4a02995ca75f2872284976c6c91d6
Andrew Ray <alr2569@gmail.com>
2015-10-03 11:19:53 -0400

    Add stub to controller for ADB

--- controller.js
+++ controller.js
@@ -7,15 +7,18 @@ app.controller("controllerTimeline", function ($scope, $http) {
     $scope.filter = "";
     $scope.zoomIn = function () {
         if ($scope.zoom !== maxZoom) $scope.zoom++;
-    }
+    };
     $scope.zoomOut = function () {
         'use strict';
         if ($scope.zoom  !== 0) $scope.zoom--;   
-    }
+    };
     $http.get("data.json").success( function(response) {
         'use strict';
         $scope.events = response.events;
     });
+    $scope.mouseEvent = function() {
+        
+    };
 });
 
 window.onload();
