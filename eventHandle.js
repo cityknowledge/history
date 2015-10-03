c4b9fec076b85b8d2ecd1fa2ae0ba537a4f9f7f8
unknown <adbeers@GL2-20.admin.wpi.edu>
2015-10-03 11:51:38 -0400

    Working on the slider rectangle

---
+++ eventHandle.js
@@ -0,0 +1,18 @@
+//File which defines the slider
+
+
+//Class which defines the shape of a rectangle
+function Rectangle(x , y, w, h, fill) {
+this.x = x || 0;
+this.y = y || 0;
+this.w = w || 1;
+this.h = h || 1;
+this.fill = fill || '#000000';
+
+} 
+
+
+function onAxisClick(){
+
+
+} 
