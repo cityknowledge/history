58da5d03493060de9b6a3dfdbc929a4987ab08c6
alray2569 <alr2569@gmail.com>
2015-10-04 21:42:22 -0400

    IE 9?/10?/11 bughandling
    Personally invite IE to scroll at the same rate as other browsers and
    not 120x slower. No, really: why does IE divide the scroll distance by
    120 and then expect the input not to be divided by 120?
    Force IE to refresh the canvas on window resize. As far as I can tell,
    the only way to do this is to reload the page.

--- scroll.js
+++ scroll.js
@@ -1,4 +1,6 @@
 function scroll(event) {
     'use strict';
-    window.scrollBy(event.deltaY || event.wheelDeltaY, 0);
+	var isIE = ((navigator.appName == 'Microsoft Internet Explorer') || ((navigator.appName == 'Netscape') && (new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null)));
+    if (!isIE) { window.scrollBy(event.deltaY || event.wheelDeltaY, 0); }
+	if (isIE) { window.scrollBy(event.deltaY * 120, 0); }
 }
--- windowattr.js
+++ windowattr.js
@@ -6,6 +6,10 @@ window.onload = function () {
 
 window.onresize = function () {
     'use strict';
+	var isIE = ((navigator.appName == 'Microsoft Internet Explorer') || ((navigator.appName == 'Netscape') && (new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null)));
+    if (isIE) {
+		window.location.reload(true);
+	}
     size();
     draw();
 };
