beb7b0368939acf5ad43bb87418f6121e54243f1
Andrew Ray <alr2569@gmail.com>
2015-10-28 17:42:08 +0100

    Cleanup

--- interceptWheel.js
+++ interceptWheel.js
@@ -3,6 +3,8 @@ This code is taken from the Mozilla developer wiki page at https://developer.moz
 This code is written by Mozilla Contributors, and is available under the CC-0 license, available at http://creativecommons.org/licenses/zero/1.0
 */
 
+/*jslint nomen: true*/
+
 (function (window, document) {
     'use strict';
     var prefix = "", _addEventListener, onwheel, support;
@@ -18,20 +20,20 @@ This code is written by Mozilla Contributors, and is available under the CC-0 li
     // detect available wheel event
     support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
               document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
-              "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
+                    "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
 
-    window.addWheelListener = function( elem, callback, useCapture ) {
-        _addWheelListener( elem, support, callback, useCapture );
+    window.addWheelListener = function (elem, callback, useCapture) {
+        _addWheelListener(elem, support, callback, useCapture);
 
         // handle MozMousePixelScroll in older Firefox
-        if( support == "DOMMouseScroll" ) {
-            _addWheelListener( elem, "MozMousePixelScroll", callback, useCapture );
+        if (support === "DOMMouseScroll") {
+            _addWheelListener(elem, "MozMousePixelScroll", callback, useCapture);
         }
     };
 
-    function _addWheelListener( elem, eventName, callback, useCapture ) {
-        elem[ _addEventListener ]( prefix + eventName, support == "wheel" ? callback : function( originalEvent ) {
-            !originalEvent && ( originalEvent = window.event );
+    function _addWheelListener(elem, eventName, callback, useCapture) {
+        elem[_addEventListener](prefix + eventName, support === "wheel" ? callback : function (originalEvent) {
+            !originalEvent && (originalEvent = window.event);
 
             // create a normalized event object
             var event = {
@@ -39,31 +41,29 @@ This code is written by Mozilla Contributors, and is available under the CC-0 li
                 originalEvent: originalEvent,
                 target: originalEvent.target || originalEvent.srcElement,
                 type: "wheel",
-                deltaMode: originalEvent.type == "MozMousePixelScroll" ? 0 : 1,
+                deltaMode: originalEvent.type === "MozMousePixelScroll" ? 0 : 1,
                 deltaX: 0,
                 deltaZ: 0,
-                preventDefault: function() {
+                preventDefault: function () {
                     originalEvent.preventDefault ?
-                        originalEvent.preventDefault() :
-                        originalEvent.returnValue = false;
+                            originalEvent.preventDefault() :
+                            originalEvent.returnValue = false;
                 }
             };
             
             // calculate deltaY (and deltaX) according to the event
-            if ( support == "mousewheel" ) {
-                event.deltaY = - 1/40 * originalEvent.wheelDelta;
+            if (support === "mousewheel") {
+                event.deltaY = -1 / 40 * originalEvent.wheelDelta;
                 // Webkit also support wheelDeltaX
-                originalEvent.wheelDeltaX && ( event.deltaX = - 1/40 * originalEvent.wheelDeltaX );
+                originalEvent.wheelDeltaX && (event.deltaX = -1 / 40 * originalEvent.wheelDeltaX);
             } else {
                 event.deltaY = originalEvent.detail;
             }
 
             // it's time to fire the callback
-            return callback( event );
+            return callback(event);
 
-        }, useCapture || false );
+        }, useCapture || false);
     }
 
-})(window,document);
-
-null;
+})(window, document);
