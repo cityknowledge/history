4887fca85db8f7b94c1849346fe96512150c709c
Andrew Ray <alr2569@gmail.com>
2015-10-30 14:50:08 +0100

    Prevent dragging outside of canvas from selecting text

--- windowattr.js
+++ windowattr.js
@@ -10,6 +10,14 @@ window.controllerLoad = function () {
     window.setInterval(function () {
         window.scrollBy(scrollVal, 0);
     }, 5);
+    var canvasEls = document.getElementsByTagName('canvas'),
+        preventHl = function () { return false; },
+        i = 0;
+
+    while (i < canvasEls.length) {
+        canvasEls[i].onmousedown = preventHl;
+        i += 1;
+    }
 };
 
 window.onresize = function () {
