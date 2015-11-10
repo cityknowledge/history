5aa35ab779f13076583deee7480dc2768ca69e96
Andrew Ray <alr2569@gmail.com>
2015-11-10 14:53:41 +0100

    Resize things to fit the new canvas The new time axis system will take up more space on the page, so other things needed to be resized to make it fit. This has been done now. Additionally, most of the sizing is done within the css file now, using the calc() object, rather than in a JS function. This should save computation time, since the function only needs to resize the canvas now (this cannot be done in css because of the way that canvases work).

--- js/size.js
+++ js/size.js
@@ -7,26 +7,7 @@ function size() {
         d = document,
         e = d.documentElement,
         g = $('body')[0],
-        t = w.innerWidth || e.clientWidth || g.clientWidth,
-        y = w.innerHeight || e.clientHeight || g.clientHeight,
-        n = y - 280,
-        h = n - 140;
+        t = w.innerWidth || e.clientWidth || g.clientWidth;
     
-    /*
-    for (x = 0; x < articles.length; x++) {
-        articles[x].style.height = n.toString() + "px";
-        articles[x].setAttribute("style", "height: " + n.toString() + "px;");
-    }
-    
-    canvas[0].setAttribute("width", t.toString());
-    
-    for (x = 0; x < imgs.length; x++) {
-        imgs[x].setAttribute("style", "max-height: " + h.toString() + "px;");
-    }*/
-    
-    
-    $("article").css('height', n.toString() + "px");
-    $(".arrow").css("height", n.toString() + "px");
     $("canvas")[0].setAttribute("width", t.toString());
-    $("img.ipimg").css("max-height", h.toString() + "px");
 }
