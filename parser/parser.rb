11696c8ee2d90b28015860170c1dbcd60c8eb4de
Mistymush <beersaugust@gmail.com>
2015-11-17 22:08:00 +0100

    Fixed parser bugs, including, Image discription text Dates with no day but a month Capitalizing events Sentury description events

--- parser/parser.rb
+++ parser/parser.rb
@@ -23,16 +23,16 @@ $events = Array.new
 $current_event = Event.new
 #initialize first page and year of text extraction
 #note these value are one less than the actual starts
-$current_page = '24'
-$current_year = '421'
+$current_page = '23'
+$current_year = '400'
 
 $line = '0'
 
 #Boolean to control ratchet miss page numberings
 $flip = true
 
-
-File.open('A25-844.txt', 'r:utf-8').each_line do |line|
+#begin parse algorithm
+File.open('AD_no_image_text.txt', 'r:utf-8').each_line do |line|
 
   #if a number that is the only text on a line
   if(/\A\d+\Z/.match(line))
@@ -54,7 +54,7 @@ File.open('A25-844.txt', 'r:utf-8').each_line do |line|
         $flip = true
 
       end
-      #puts $current_page
+      puts $current_page
 
 
     elsif(line.to_i - $current_year.to_i < 40 && line.to_i - $current_year.to_i > 0 )
@@ -67,8 +67,8 @@ File.open('A25-844.txt', 'r:utf-8').each_line do |line|
   #At this point we have found content that is part of an event
 
   #Event start with a date
-  elsif(/\A\.(?<date>(\d+\s{1}\w+))/.match(line))
-    data = /\A\.(?<date>(\d+\s{1}\w+))/.match(line)
+  elsif(/\A\.(?<date>((\d+\s{1}\w+)|(\w+))):/.match(line))
+    data = /\A\.(?<date>((\d+\s{1}\w+)|(\w+))):/.match(line)
     $current_event
     $events << $current_event
     $current_event = Event.new($current_year)
@@ -77,7 +77,9 @@ File.open('A25-844.txt', 'r:utf-8').each_line do |line|
 
     #if the line contains more information than just the date it is content.
     if(line.length - (data[:date].length + 3) > 0)
-      $current_event.content =  line[data[:date].length + 3, (line.length - (data[:date].length + 4))] + ' '
+        tmp_content = line[data[:date].length + 3, (line.length - (data[:date].length + 4))] + ' '
+        tmp_content = tmp_content.slice(0,1).capitalize + tmp_content.slice(1..-1)
+        $current_event.content =  tmp_content
 
     else
       $current_event.content = ''
@@ -89,27 +91,32 @@ File.open('A25-844.txt', 'r:utf-8').each_line do |line|
     $events << $current_event
     $current_event = Event.new($current_year)
     $current_event.page = $current_page
-    $current_event.content =  line[1, line.length - 2] + ' '
+      
+      tmp_content = line[1, line.length - 2] + ' '
+      tmp_content = tmp_content.slice(0,1).capitalize + tmp_content.slice(1..-1)
+      $current_event.content = tmp_content
 
   #mid event content
   else
 
     if((!/([[:graph:]]+)\s([[:graph:]]+)\s([[:graph:]]+)\Z/.match(line)))
-      puts $current_page
-      puts line
+      #puts $current_page
+      #puts line
     end
 
-    $current_event.content = $current_event.content + line[0, line.length - 2] + ' '
+    $current_event.content = $current_event.content + line[0, line.length - 1] + ' '
 
   end
 
 end
+
+
 #add last event ot event list
 $events << $current_event
 
 #now print the result of parsing
 out_file = File.new('JSON.txt', 'w:utf-8')
-
+num = 0
 out_file.print "{\n"
 out_file.print '  "events: ["' + "\n"
 
@@ -120,8 +127,10 @@ $events.each do |event|
   out_file.print '      "Content" : "' + event.content + %Q[",\n]
   out_file.print '      "Location" : "' + %Q[",\n]
   out_file.print '      "Filter" : "' + %Q[",\n]
+  out_file.print '      "UID" : "' + num.to_s + %Q[",\n]  
   out_file.print %Q[      "Citation" : "Distefano, Giovanni. L'atlante Storico Di Venezia. Venice, Italy: Supernova Edizioni srl, 2007. ] + event.page + %Q["\n]
   out_file.print "    },\n"
+    num += 1
 end
 
 out_file.print "  ]\n"
