4e72000d1fe0600242e00d8f14d8545fb5d575cc
Andrew Ray <alr2569@gmail.com>
2015-12-02 13:19:37 +0100

    Rewrite bookmarks to merge with stickies.

---
+++ js/bookmarks2.js
@@ -0,0 +1,87 @@
+/*jshint browser: true*/
+/*global $*/
+
+function addBookmark(group, key, text) {
+    'use strict';
+    var ls = JSON.parse(localStorage.getItem("history") || "{}"),
+        g = ls[group] || {};
+    
+    g[key] = text || "";
+    ls[group] = g;
+    localStorage.setItem("history", JSON.stringify(ls));
+}
+
+function remBookmark(group, key) {
+    'use strict';
+    var g,
+        ls = JSON.parse(localStorage.getItem("history") || "{}");
+    
+    g = ls[group];
+    if (g) {
+        if (g[key]) {
+            delete g[key];
+            ls[group] = g;
+            if (Object.keys(ls[group]).length === 0) {
+                delete ls[group];
+            }
+            localStorage.setItem("history", JSON.stringify(ls));
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+function getBookmarks(group) {
+    'use strict';
+    var ls = JSON.parse(localStorage.getItem("history") || "{}"),
+        g = ls[group] || {},
+        keys = Object.keys(g);
+    return keys;
+}
+
+function getBookmarkText(group, key) {
+    'use strict';
+    var ls = JSON.parse(localStorage.getItem("history") || "{}"),
+        g = ls[group] || {},
+        text = g[key] || "";
+    return text;
+}
+
+function newGroup() {
+    'use strict';
+    var ls = JSON.parse(localStorage.getItem("history") || "{}"),
+        name = window.prompt("Nome", "");
+    ls[name] = {};
+    localStorage.setItem("history", JSON.stringify(ls));
+}
+
+function getGroups() {
+    'use strict';
+    return Object.keys(JSON.parse(localStorage.getItem("history") || "{}"));
+}
+
+function getAllBookmarks() {
+    'use strict';
+    var group, key,
+        array = [],
+        ls = JSON.parse(localStorage.getItem("history") || "{}"),
+        groups = Object.keys(ls);
+    
+    for (group in groups) {
+        for (key in Object.keys(ls[group])) {
+            if (array.indexOf(key) < 0 ) {
+                array.push(key);
+            }
+        }
+    }
+    return array;
+}
+
+function hasBookmark(group, key) {
+    'use strict';
+    var ls = JSON.parse(localStorage.getItem("history") || "{}"),
+        g = ls[group] || {};
+    
+    return g[key] !== undefined;
+}
---
+++ js/sticky2.js
@@ -0,0 +1,56 @@
+/*jshint browser:true*/
+/*global $, getGroups, hasBookmark, getBookmarkText*/
+
+function stickyText(text) {
+    document.getElementById("stickytext").innerHTML = text;
+}
+
+function stickyButtons(exists, group, key) {
+    if (exists) {
+        document.getElementById("stickybuttons").innerHTML =
+            "<button onclick=\"addBookmark(&quot;" + group + "&quot;,&quot;" + key + "&quot;, document.getElementById(&quot;stickytext&quot;).value);closeSticky();\">Tenere</button><button onclick=\"closesticky()\">Annulare</button><button onclick=\"remBookmark(&quot;" + group + "&quot;,&quot;" + key + "&quot;);closeSticky();\">Cancellare</button>";
+    } else {
+        document.getElementById("stickybuttons").innerHTML =
+            "<button onclick=\"addBookmark(&quot;" + group + "&quot;,&quot;" + key + "&quot;,document.getElementById(&quot;stickytext&quot;).value);closeSticky();\">Aggiungere</button><button onclick=\"closesticky();\">Annulare</button>";
+    }
+}
+
+function updateSticky(group, key) {
+    stickyText(getBookmarkText(group, key));
+    stickyButtons(hasBookmark(group, key), group, key);
+}
+
+function stickySelect() {
+    'use strict';
+    var group,
+        str = "<option value=\"\">Scegli un gruppo...</option>";
+    
+    getGroups().forEach(function (group) {
+        str += "<option value=\"" + group + "\">" + group + "</option>";
+    });
+    
+    str += "<option value=new>Nuovo...</option>";
+    
+    document.getElementById("bkgr").innerHTML = str;
+}
+
+function popsticky() {
+    'use strict';
+    var ls = JSON.parse(localStorage.getItem("history") || "{}"),
+        id = window.$scope.ipevent - 1,
+        events = window.$scope.search ? window.$filter('filter')(window.$scope.events, window.$scope.getFilter()) : window.$scope.events,
+        key = events[id].key,
+        text = ls[key] || "";
+    
+    document.getElementById("sticky").innerHTML = 
+        "<h3>Le sue note su quest’evento</h3><p>Potrà leggere queste note dopo; non saranno disponibili al pubblico.</p><select id=bkgr onchange=\"if(document.getElementById(&quot;bkgr&quot;).value===&quot;new&quot;){newGroup();stickySelect();}else{updateSticky(document.getElementById(&quot;bkgr&quot;).value,&quot;" + key + "&quot;)}\"></select><br><textarea id=stickytext cols=50 rows=20>" + text + "</textarea><br><div id=stickybuttons></div>";
+    
+    stickySelect();
+    
+    $("#sticky").css("display", "block");
+}
+
+function closeSticky() {
+    'use strict';
+    $("#sticky").css("display", "none");
+}
