de1a59b3cf167376e337001a6e105f453be1a861
Andrew Ray <alr2569@gmail.com>
2015-12-07 16:44:17 +0100

    Change to use push

--- js/flagworker.js
+++ js/flagworker.js
@@ -4,5 +4,5 @@
 this.onmessage = function(message) {
     importScripts("https://cdn.firebase.com/js/client/2.3.1/firebase.js");
     var FB = new Firebase("https://venicedata.firebaseio.com/history_flags/" + message.data.key);
-    FB.set(message.data.text);
+    FB.push(message.data.text);
 };
