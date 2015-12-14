671ea23fe7f2e7bb2497fc403e732ef5a65c6b20
Andrew Ray <alr2569@gmail.com>
2015-12-14 16:48:56 +0100

    Randomize JSON

--- serv/randomizer.js
+++ serv/randomizer.js
@@ -7,14 +7,14 @@ fs.readFile("../data.json", "utf-8", function (error, data) {
 		console.log(error);
 		process.exit(0xBADC0DE0);
 	} else {
-		while (data.indexOf("\"Count\": 1,") >= 0) {
-			data = data.replace("\"Count\": 1,", "\"Count\": 0,");
+		while (data.indexOf("\"Count\":1,") >= 0) {
+			data = data.replace("\"Count\":1,", "\"Count\":0,");
 		}
 		while (data.indexOf("\"Count\": 2,") >= 0) {
-			data = data.replace("\"Count\": 2,", "\"Count\": 0,");
+			data = data.replace("\"Count\":2,", "\"Count\":0,");
 		}
-		while (data.indexOf("\"Count\": 0,") >= 0) {
-			data = data.replace("\"Count\": 0,", "\"Count\": " + Math.floor((Math.random() * 10) + 1).toString() + ",");
+		while (data.indexOf("\"Count\":0,") >= 0) {
+			data = data.replace("\"Count\":0,", "\"Count\":" + Math.floor((Math.random() * 10) + 1).toString() + ",");
 		}
 		console.log(data);
 		process.exit(0xA115600D);
