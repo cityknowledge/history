83e22872547ed6f2f53084861405368134333c59
Andrew Ray <alr2569@gmail.com>
2015-12-17 15:08:42 +0100

    Update rssparser to push to correct node

--- serv/rssparser.js
+++ serv/rssparser.js
@@ -28,7 +28,7 @@ function main2() {
     event.Content = event.Content.slice(0, event.Content.indexOf("<br /><br /><br />"));
     event.Content = event.Content.replace(/<[^>]+>/g, " ");
     console.log(event);
-    FB.child("history_testing").child(feed.items[0].date.toString().replace(/ |\.|#|$|\[|\]/g, "")).update(event, function (error) {
+    FB.child("history").child(feed.items[0].date.toString().replace(/ |\.|#|$|\[|\]/g, "")).update(event, function (error) {
         if (error) {
             console.log(error);
         } else {
